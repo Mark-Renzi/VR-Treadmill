4b0482357a29959d4ea967076c3ce5449ce3fbb8
Mark Renzi <mrenzi402@gmail.com>
2025-10-19 23:30:36 -0400

    make the bar look nicer

--- vr_treadmill/ui_resources/joystick_bar.py
+++ vr_treadmill/ui_resources/joystick_bar.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QWidget
-from PyQt6.QtGui import QPainter, QColor, QBrush
+from PyQt6.QtGui import QPainter, QColor, QBrush, QPen
 from PyQt6.QtCore import Qt
 
 
@@ -7,7 +7,7 @@ class JoystickBar(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.value = 0  # Range: -32768 to 32767
-        self.setMinimumHeight(20)
+        self.setMinimumHeight(25)
 
     def set_value(self, value):
         """Update the bar value and trigger repaint."""
@@ -22,15 +22,37 @@ class JoystickBar(QWidget):
         height = self.height()
         center = width // 2
 
+        # Draw filled bar
         normalized = self.value / 32768
 
         if normalized > 0:
-            bar_width = int(center * normalized)
-            painter.setPen(0)
+            bar_width = int((center -1) * normalized)
+            painter.setPen(Qt.PenStyle.NoPen)
             painter.setBrush(QBrush(QColor(0, 180, 0)))
-            painter.drawRect(center, 0, bar_width, height)
+            painter.drawRect(center, 2, bar_width, height - 4)
         elif normalized < 0:
-            bar_width = int(center * abs(normalized))
-            painter.setPen(0)
+            bar_width = int((center -1) * abs(normalized))
+            painter.setPen(Qt.PenStyle.NoPen)
             painter.setBrush(QBrush(QColor(180, 0, 0)))
-            painter.drawRect(center - bar_width, 0, bar_width, height)
+            painter.drawRect(center - bar_width, 2, bar_width, height - 4)
+
+        # Draw outer frame
+        frame_pen = QPen(self.palette().text().color())
+        frame_pen.setWidth(1)
+        painter.setPen(frame_pen)
+        painter.setBrush(Qt.BrushStyle.NoBrush)
+        painter.drawRoundedRect(1, 1, width-2, height-2, 4, 4)
+
+        # Draw center line
+        lighter_color = self.palette().text().color()
+        lighter_color.setAlpha(30)
+        painter.setPen(QPen(lighter_color, 1, Qt.PenStyle.DotLine))
+        painter.drawLine(center, 0, center, height)
+
+        # Draw the value as text
+        font = painter.font()
+        font.setBold(True)
+        painter.setFont(font)
+        painter.setPen(self.palette().text().color())
+        text = str(self.value)
+        painter.drawText(self.rect(), Qt.AlignmentFlag.AlignCenter, text)
